29d064c57b6bc01d25a0064c3bc26386d057d70d
12115838 <lucaas.daniel2003@gmail.com>
2023-03-07 20:31:24 -0300

    progama em java

---
+++ src/br/com/newton/linguagem/aula03/Contato.java
@@ -0,0 +1,6 @@
+package br.com.newton.linguagem.aula03;
+
+public class Contato {
+        public String nome;
+        public String numero;
+}
---
+++ src/br/com/newton/linguagem/aula03/Telefone.java
@@ -0,0 +1,6 @@
+package br.com.newton.linguagem.aula03;
+
+public class Telefone {
+  Contato contato1;
+
+}
